c1d4ba62e78d57fb2ab86b30a7d9569d7d492ba5
Dartkoo <119898079+Dartkoo@users.noreply.github.com>
2022-12-10 13:29:40 -0600

    Update pelismax.php

--- pelismax.php
+++ pelismax.php
@@ -3,9 +3,9 @@ if ($_GET["token"] == "pelismax") {
 $id=$_GET['id'];
 header ("Content-Type: application/x-mpegURL; charset=UTF-8");
 header("HTTP/1.1 302 Found"); 
-header('Location: http://regioplay.xyz/php/SR_REGIO_220122_/tpnuevo01.php?c=' .$id. '&tokennn=Regio&f=.m3u8');
+header('Location: https://regioplay.xyz/php/SR_REGIO_502122_/tpnuevo02.php?tokennn=Regio&c=' .$id. '&f=.m3u8');
 } else {
-//Aqui pones el codigo de los eventos que ocurriran si la contraseña es incorrecta o si el usuario es incorrecto.
+//Aqui pones el codigo de los eventos que ocurriran si la contraseÃ±a es incorrecta o si el usuario es incorrecto.
 echo $_GET["user"] . ' Denied access.';
 }
 ?>
